c9a313ebaadd535321573fab0e3364babe183c18
Doug Townsend <townsend.doug@gmail.com>
2025-10-15 13:53:45 -0400

    added hw4 solution

--- hw4/problem3/xor2_tb.v
+++ hw4/problem3/xor2_tb.v
@@ -1,7 +1,25 @@
 `timescale 1ns/100ps
 module xor2_tb();
     //Your code here
+    reg a, b;
+    wire f;
+    xor2 uut(.a(a), .b(b), .f(f));
+
     initial begin
+        $dumpfile("xor2_tb.vcd");
+        $dumpvars(0, xor2_tb);
+        a = 0;
+        b = 0;
+        #5
+        a = 0;
+        b = 1;
+        #5
+        a = 1;
+        b = 0;
+        #5
+        a = 1;
+        b = 1;
+        #5
         $finish;
     end
 endmodule
